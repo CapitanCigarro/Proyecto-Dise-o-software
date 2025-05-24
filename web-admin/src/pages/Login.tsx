4d82abe6f3a716713c7e4ccf5fb9f40f171d6274
jodoy2022 <jgodoy2022@udec.cl>
2025-05-24 13:56:14 -0400

    base for the web-admin

---
+++ web-admin/src/pages/Login.tsx
@@ -0,0 +1,15 @@
+// src/pages/Login.tsx
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login WebAdmin</h1>
+      <input type="text" placeholder="Usuario" />
+      <input type="password" placeholder="Contraseña" />
+      <button>Iniciar sesión</button>
+    </div>
+  );
+};
+
+export default Login;
